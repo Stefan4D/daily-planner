924a9969ada2d212e9d9f549ea7f5df969fd3b25
Stefan <blockchain.crypto.stefan@gmail.com>
2023-08-04 19:49:50 +0100

    Added save state notification; Added event listener to save events

--- README.md
+++ README.md
@@ -17,3 +17,13 @@ The app should:
 - Allow a user to enter an event when they click a timeblock.
 - Save the event in local storage when the save button is clicked in that timeblock.
 - Persist events between refreshes of a page.
+
+## Resources used
+
+- [JQuery Docs: Siblings](https://api.jquery.com/siblings/)
+
+## Learning Outcomes
+
+- I learned how to use the JQuery core and UI libraries to rapidly prototype applications
+- I learned about the .siblings() method in JQuery, which is a very useful way to grab an element in the DOM that is related to the current item (using the this keyword), which allowed me to succinctly delegate events and save changes made by the user
+- I revisited flexbox in order to get the content to display correctly and mimic the example provided
--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,21 @@
 // - Save the event in local storage when the save button is clicked in that timeblock.
-const testEvents = {
-  "9AM": "The 9am event",
-  "10AM": "test",
-  "11AM": "test",
-  "12PM": "test",
-  "1PM": "test",
-  "2PM": "test",
-  "3PM": "test",
-  "4PM": "test",
-  "5PM": "The 5pm event",
-  "6PM": "Write some JQuery",
-  "7PM": "Debug my JQuery",
-  "8PM": "Run away from JQuery",
-  "9PM": "test",
-  "10PM": "test",
-};
-localStorage.setItem("events", JSON.stringify(testEvents));
+// const testEvents = {
+//   "9AM": "The 9am event",
+//   "10AM": "test",
+//   "11AM": "test",
+//   "12PM": "test",
+//   "1PM": "test",
+//   "2PM": "test",
+//   "3PM": "test",
+//   "4PM": "test",
+//   "5PM": "The 5pm event",
+//   "6PM": "Write some JQuery",
+//   "7PM": "Debug my JQuery",
+//   "8PM": "Run away from JQuery",
+//   "9PM": "test",
+//   "10PM": "test",
+// };
+// localStorage.setItem("events", JSON.stringify(testEvents));
 const savedEvents = JSON.parse(localStorage.getItem("events")) || {};
 
 // - Display the current day at the top of the calendar when a user opens the planner.
@@ -27,23 +27,7 @@ currentDay.text(today);
 
 // - Present timeblocks for standard business hours when the user scrolls down.
 const container = $(".container");
-
-// const row = $("<div>");
-// const textArea = $("<textarea>");
-// const saveButton = $("<div>");
-// const hour = $("<div>");
-// row.addClass("row");
-// textArea.addClass("description present");
-// textArea.text("test");
-// saveButton.addClass("saveBtn");
-// saveButton.text("Save");
-// hour.addClass("hour");
-// hour.text("9AM");
-
-// row.append(hour);
-// row.append(textArea);
-// row.append(saveButton);
-// container.append(row);
+const saveState = $("#save-state");
 
 // for loop to cycle through hours from 9AM to 5PM using Day.js
 for (let i = 9; i < 22; i++) {
@@ -68,11 +52,14 @@ for (let i = 9; i < 22; i++) {
 
   // need to use the formatted time here to match the savedEvents object keys
   textArea.text(savedEvents[formattedTime] || "");
+  textArea.attr("id", formattedTime);
 
   saveButton.addClass("saveBtn");
+  saveButton.attr("data-time", formattedTime);
   //   saveButton.text("Save");
   // add font awesome icon to save button
   saveButton.append('<i class="fas fa-save"></i>');
+
   hour.addClass("hour");
   // using formatted time here for display
   hour.text(formattedTime);
@@ -82,6 +69,23 @@ for (let i = 9; i < 22; i++) {
   row.append(saveButton);
   container.append(row);
 }
+
+// originally used the .savBtn class to target the save button, but this made the entire div the save button
+// so changed to use the font awesome icon instead
+// changed event handler to use .parent() to get the correct element as the icon is the target but has no siblings so need to go up to the parent
+const saveBtn = $(".fa-save");
+
+// have to use function here to get the correct 'this' context
+saveBtn.on("click", function () {
+  // need to use .parent() to get the correct elements
+  const textArea = $(this).parent().siblings(".description");
+  const hour = $(this).parent().siblings(".hour");
+  const time = hour.text();
+  const event = textArea.val();
+  savedEvents[time] = event;
+  localStorage.setItem("events", JSON.stringify(savedEvents));
+  saveState.text("Saved!");
+});
 
 // implement a loop to go from 9am to 5pm
 // add a .row for each hour timeslot
