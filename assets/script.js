16bc0fdb74933418285dc04910324eb2991a653c
Stefan <blockchain.crypto.stefan@gmail.com>
2023-08-04 19:12:47 +0100

    Added localStorage and test data

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,23 @@
+// - Save the event in local storage when the save button is clicked in that timeblock.
+const testEvents = {
+  "9AM": "The 9am event",
+  "10AM": "test",
+  "11AM": "test",
+  "12PM": "test",
+  "1PM": "test",
+  "2PM": "test",
+  "3PM": "test",
+  "4PM": "test",
+  "5PM": "The 5pm event",
+  "6PM": "Write some JQuery",
+  "7PM": "Debug my JQuery",
+  "8PM": "Run away from JQuery",
+  "9PM": "test",
+  "10PM": "test",
+};
+localStorage.setItem("events", JSON.stringify(testEvents));
+const savedEvents = JSON.parse(localStorage.getItem("events")) || {};
+
 // - Display the current day at the top of the calendar when a user opens the planner.
 // using this plugin to allow for ordinal to be included (1st, 2nd, 3rd etc)
 dayjs.extend(window.dayjs_plugin_advancedFormat);
@@ -26,19 +46,36 @@ const container = $(".container");
 // container.append(row);
 
 // for loop to cycle through hours from 9AM to 5PM using Day.js
-for (let i = 9; i < 18; i++) {
-  const time = dayjs().hour(i).minute(0).second(0).format("hA");
+for (let i = 9; i < 22; i++) {
+  // cannot use .format() here otherwise it interferes with the comparison below
+  const now = dayjs();
+  const time = dayjs().hour(i).minute(0).second(0);
+  const formattedTime = time.format("hA");
   const row = $("<div>");
   const textArea = $("<textarea>");
   const saveButton = $("<div>");
   const hour = $("<div>");
   row.addClass("row");
-  textArea.addClass("description present");
-  textArea.text("test " + time);
+
+  // compares the 2 Day.js objects without formatting
+  if (time.isSame(now, "hour")) {
+    textArea.addClass("description present");
+  } else if (now.isAfter(time)) {
+    textArea.addClass("description past");
+  } else {
+    textArea.addClass("description future");
+  }
+
+  // need to use the formatted time here to match the savedEvents object keys
+  textArea.text(savedEvents[formattedTime] || "");
+
   saveButton.addClass("saveBtn");
-  saveButton.text("Save");
+  //   saveButton.text("Save");
+  // add font awesome icon to save button
+  saveButton.append('<i class="fas fa-save"></i>');
   hour.addClass("hour");
-  hour.text(time);
+  // using formatted time here for display
+  hour.text(formattedTime);
 
   row.append(hour);
   row.append(textArea);
@@ -55,7 +92,5 @@ for (let i = 9; i < 18; i++) {
 // - Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 
 // - Allow a user to enter an event when they click a timeblock.
-
-// - Save the event in local storage when the save button is clicked in that timeblock.
 
 // - Persist events between refreshes of a page.
